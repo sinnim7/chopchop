72c031837a2f96d2c10357d08b00ccb2425debc7
sinnim7 <sinnim7@gmail.com>
2019-10-24 18:49:36 +0900

    v7

---
+++ v7/src/main/java/chop/domain/Member.java
@@ -0,0 +1,89 @@
+package chop.domain;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+  private static final long serialVersionUID = 1L;
+  
+  private int no;
+  private String password;
+  private String email;
+  private String photo;
+  private String nickname;
+  private String tel;
+  private String postNo;
+  private String baseAddress;
+  private String detailAddress;
+  private int grade;
+  
+  
+  
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getPhoto() {
+    return photo;
+  }
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
+  public String getNickname() {
+    return nickname;
+  }
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+  public String getTel() {
+    return tel;
+  }
+  public void setTel(String tel) {
+    this.tel = tel;
+  }
+  public String getPostNo() {
+    return postNo;
+  }
+  public void setPostNo(String postNo) {
+    this.postNo = postNo;
+  }
+  public String getBaseAddress() {
+    return baseAddress;
+  }
+  public void setBaseAddress(String baseAddress) {
+    this.baseAddress = baseAddress;
+  }
+  public String getDetailAddress() {
+    return detailAddress;
+  }
+  public void setDetailAddress(String detailAddress) {
+    this.detailAddress = detailAddress;
+  }
+  public int getGrade() {
+    return grade;
+  }
+  public void setGrade(int grade) {
+    this.grade = grade;
+  }
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+  
+  
+  
+
+  
+}
